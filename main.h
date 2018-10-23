856b8a07a2c29287499cd37c36c4fa216b24b147
Justin Reina <justinmreina@gmail.com>
2018-10-23 10:30:42 -0700

    (C)    main cleanup & jreina format apply

---
+++ main.h
@@ -0,0 +1,36 @@
+/************************************************************************************************************************************/
+/** @file		main.h
+ * 	@brief		x
+ * 	@details	x
+ *
+ * 	@author		Justin Reina, Firmware Engineer, Company Name
+ * 	@created	Oct 23, 2018
+ * 	@last rev	x
+ *
+ *
+ * 	@notes		x
+ *
+ * 	@section	Opens
+ * 			none current
+ *
+ * 	@section	Legal Disclaimer
+ * 			© Year Company Name, All rights reserved. All contents of this source file and/or any other related source
+ *			files are the explicit property of Company Name. Do not distribute. Do not copy.
+ */
+/************************************************************************************************************************************/
+#ifndef MAIN_H_
+#define MAIN_H_
+
+//Lib
+#include <stddef.h>
+
+//Project
+#include "globals.h"
+#include "stm32l0xx.h"
+
+//Definitions
+
+//Typedefs
+
+#endif /* MAIN_H_ */
+
